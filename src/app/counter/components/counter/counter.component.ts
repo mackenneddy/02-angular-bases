3f453c7180e451dd2cd8e4ca44c2f9d84f1d9f49
DESKTOP-RIRNKLS\kenmack <mack.melendez@tkambio.us>
2024-03-05 11:35:24 -0500

    Fin de seccion 4

---
+++ src/app/counter/components/counter/counter.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css'
+})
+export class CounterComponent {
+
+  public counter: number = 100;
+
+  increaseBy(value:number):void{
+
+    this.counter += value;
+  }
+
+  reset():void{
+    this.counter = 10;
+  }
+}
